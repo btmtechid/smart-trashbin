39783686f08a9e67471f6d77adf3f96e7d3add65
Btm Tech Id <btmtechid@gmail.com>
2025-05-30 15:02:01 +0700

    Update trashbin-esp32.ino

--- trashbin-esp32.ino
+++ trashbin-esp32.ino
@@ -1,38 +1,99 @@
-v#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Servo.h>
 
-#define LED 2
-#define Buzzer 3
-#define Sensor A1
+// WiFi dan Telegram
+const char* ssid = "NAMA_WIFI";
+const char* password = "PASSWORD_WIFI";
+const char* botToken = "TOKEN_BOT_TELEGRAM";
+const char* chatID = "CHAT_ID_KAMU";
+
+// HC-SR04 pin
+#define trigPin1 23
+#define echoPin1 22
+#define trigPin2 21
+#define echoPin2 19
+
+// LED
+#define led1 18
+#define led2 5
+
+// Servo
+#define servoPin 17
+Servo servo;
+
+long duration1, distance1;
+long duration2, distance2;
 
 void setup() {
-  Serial.begin(9200);
-  lcd.init();
-  lcd.backlight();
-  pinMode(LED, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
+  Serial.begin(115200);
+
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+  servo.attach(servoPin);
+  servo.write(0); // Tutup
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connected");
 }
 
 void loop() {
-  int value = analogRead(Sensor);
-  lcd.setCursor(0, 0); 
-  lcd.print("Value :");
-  lcd.print(value);
-  lcd.print("  ");
-
-  if (value > 700) {
-    digitalWrite(LED, HIGH);
-    tone(Buzzer,500);
-    lcd.setCursor(0, 1);
-    lcd.print("GAS Detected!");
-    Serial.println("Gas Detected");
+  // Sensor depan
+  distance1 = readDistance(trigPin1, echoPin1);
+  if (distance1 <= 10) {
+    digitalWrite(led1, HIGH);
+    servo.write(90); // buka tutup
+    delay(3000);
+    servo.write(0);  // tutup
   } else {
-    digitalWrite(LED, LOW);
-    noTone(Buzzer);
-    lcd.setCursor(0, 1);
-    lcd.print("No Gas Detected!");
-    Serial.println("No Gas");
+    digitalWrite(led1, LOW);
   }
 
-  delay(500);
+  // Sensor dalam
+  distance2 = readDistance(trigPin2, echoPin2);
+  if (distance2 <= 30) {
+    digitalWrite(led2, HIGH);
+    sendTelegram("Sampah dalam tong sudah penuh!");
+    delay(10000); // anti spam
+  } else {
+    digitalWrite(led2, LOW);
+  }
+
+  delay(1000);
+}
+
+long readDistance(int trig, int echo) {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  return pulseIn(echo, HIGH) * 0.034 / 2;
+}
+
+void sendTelegram(String message) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    String url = "https://api.telegram.org/bot" + String(botToken) +
+                 "/sendMessage?chat_id=" + String(chatID) +
+                 "&text=" + message;
+    http.begin(url);
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.println("Telegram sent");
+    } else {
+      Serial.println("Failed to send");
+    }
+    http.end();
+  }
 }
